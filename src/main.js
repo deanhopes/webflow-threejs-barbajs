7937a51ca8cd9c104929214d68a75a1a009d06af
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-07-26 09:18:25 +0100

    added cube with clear background

--- src/main.js
+++ src/main.js
@@ -1,3 +1,85 @@
 import './styles/style.css'
+import * as THREE from 'three'
+import barba from '@barba/core'
+import gsap from 'gsap'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-console.log('Hello')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+// Add Geometries
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    material
+)
+
+cube.position.set(0, 0, 0)
+
+scene.add(cube)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+scene.add(camera)
+
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(0xffffff, 0); // White background
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 0, 5);
+controls.update();
+
+// Animate
+const clock = new THREE.Clock()
+let lastElapsedTime = 0
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - lastElapsedTime
+    lastElapsedTime = elapsedTime
+
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
