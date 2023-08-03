d3dfa3d766f7b0d0ea913ce23dd065e80b64a782
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-08-03 19:25:05 +0100

    gsap block working

--- src/main.js
+++ src/main.js
@@ -1,85 +1,33 @@
 import './styles/style.css'
-import * as THREE from 'three'
-import barba from '@barba/core'
-import gsap from 'gsap'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import SplitType from 'split-type'
 
+gsap.registerPlugin(ScrollTrigger);
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Sizes
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+let typeSplit = new SplitType('.split-text', {
+    types: 'words',
+    tagName: 'span'
 })
 
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// Get the characters
+let words = document.querySelectorAll('.split-text .word');
 
-// Add Geometries
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    material
-)
+// Set initial opacity
+gsap.set(words, { opacity: 0.3 });
 
-cube.position.set(0, 0, 0)
-
-scene.add(cube)
-
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-scene.add(camera)
-
-
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
+// Set up the timeline
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#textSection',
+        start: 'top +=50%',
+        end: 'bottom bottom',
+        // pin: true,
+        scrub: 4.75,
+        markers: true
+    }
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(0xffffff, 0); // White background
-
-
-const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 0, 5);
-controls.update();
-
-// Animate
-const clock = new THREE.Clock()
-let lastElapsedTime = 0
-
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
-
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+    .to(words, {
+        opacity: 1, // the text ends at 100% opacity
+        stagger: 0.05,
+    }, 0.1);
