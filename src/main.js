4341cd20f906e4109dc39aa5015ca8b212159893
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-08-04 18:16:15 +0100

    working draggable carousel

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './styles/style.css'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import SplitType from 'split-type'
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,18 +18,42 @@ let words = document.querySelectorAll('.split-text .word');
 // Set initial opacity
 gsap.set(words, { opacity: 0.3 });
 
-// Set up the timeline
+// Set up the timeline for words opacity
 const tl = gsap.timeline({
     scrollTrigger: {
         trigger: '#textSection',
         start: 'top +=50%',
         end: 'bottom bottom',
-        // pin: true,
         scrub: 4.75,
-        markers: true
+        // markers: true
     }
 })
     .to(words, {
-        opacity: 1, // the text ends at 100% opacity
+        opacity: 1,
         stagger: 0.05,
     }, 0.1);
+
+
+var splide = new Splide('.splide', {
+    fixedWidth: '40vw',
+    fixedHeight: '40vw',
+    gap: '1rem',
+    rewind: true,
+    rewindByDrag: true,
+    perMove: 2,
+    autoplay: true,
+    interval: 3000,
+    easing: 'cubic-bezier(.42,.65,.27,.99)',
+    drag: true,
+    pagination: false,
+    infinite: true,
+});
+
+var bar = splide.root.querySelector('.carousel-progress-bar');
+splide.on('mounted move', function () {
+    var end = splide.Components.Controller.getEnd() - 1;
+    var rate = Math.min((splide.index) / end, 1);
+    bar.style.width = String(rate * 100) + '%';
+});
+
+splide.mount()
